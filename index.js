6bf7e7803cd68a6156ae68fd940134f44eb4067d
Ali Aljawad <alialjawad2k1@gmail.com>
2024-08-08 02:36:57 +0300

    added express.static for file downlaod

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ import fileRoutes from './routes/file.routes.js';
 import enrollmentRoutes from './routes/enrollments.routes.js';
 import withdrawalRoutes from './routes/withdrawal.routes.js';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
 dotenv.config();
 
 app.use(express.json());
 app.use(cors());
-
 app.use('/users', usersRoutes);
 app.use('/classes', classRoutes);
 app.use('/files', fileRoutes);
 app.use('/enrollments', enrollmentRoutes);
 app.use('/withdrawals', withdrawalRoutes);
-
+app.use('/uploads', express.static('uploads'));
 app.listen(8080, () => {
   console.log('Server running on port 8080');
   connectToDatabase();
